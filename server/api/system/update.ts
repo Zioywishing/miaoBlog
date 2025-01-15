23a824ad509da0925be3128fb25ca828fb319906
miaoSpring <539943419@qq.com>
2025-01-15 21:04:22 +0800

    fix: 修正更新系统后端api更新包路径问题

--- server/api/system/update.ts
+++ server/api/system/update.ts
@@ -6,7 +6,7 @@ export default defineEventHandler(async (event) => {
     const file = data.get('file') as File
     const buffer = await file.arrayBuffer()
     const uint8Array = new Uint8Array(buffer)
-    fs.writeFileSync('./userData/test.txt', uint8Array, {
+    fs.writeFileSync('./.output.tar.gz', uint8Array, {
         flag: 'w',
     })
     setTimeout(() => {
