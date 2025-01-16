05e6e9ec3ec64054a4e64e2f73edd81a04e0ffdd
miaoSpring <539943419@qq.com>
2025-01-17 02:24:38 +0800

    update

--- aftBuild.sh
+++ aftBuild.sh
@@ -2,8 +2,7 @@
 
 # 检查.output目录是否存在
 if [ -d ".output" ]; then
-    tar -czvf ./output.tar.gz .output
+    tar -czf ./output.tar.gz .output
     mv output.tar.gz .output/output.tar.gz
+    echo "generate output.tar.gz finish"
 fi
-
-echo "You can preview this build using node .output/server/index.mjs"
