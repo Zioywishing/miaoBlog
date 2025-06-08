996158cb2400224f362fcfdcfa4fc4ae657e9d7d
Miao_Spring <539943419@qq.com>
2025-06-08 11:46:32 +0800

    update: 尝试让GitHub Action能够构建适用于linux_amd64的版本

--- aftBuild.sh
+++ aftBuild.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 替换better_sqlite3.node文件
+if [ -f "better_sqlite3_linux_amd64.node" ] && [ -d ".output/server/node_modules/better-sqlite3/build/Release" ]; then
+    cp better_sqlite3_linux_amd64.node .output/server/node_modules/better-sqlite3/build/Release/better_sqlite3.node
+    echo "替换better_sqlite3.node文件完成"
+fi
+
 # 检查.output目录是否存在
 if [ -d ".output" ]; then
     tar -czf ./output.tar.gz .output
