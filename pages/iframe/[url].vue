4c16e611bb65611522837b904265d215bf725486
Miao_Spring <539943419@qq.com>
2025-06-09 17:51:31 +0800

    update: iframe加载时动画效果

--- pages/iframe/[url].vue
+++ pages/iframe/[url].vue
@@ -1,12 +1,30 @@
 <template>
     <div class="iframe-wrapper">
-        <iframe :src="decodeURIComponent(route.params.url as string)" frameborder="0"></iframe>
+        <iframe ref="iframeRef" :src="decodeURIComponent(route.params.url as string)" frameborder="0"></iframe>
+        <Transition name="fade">
+            <div class="loading w-full h-full bg-[#ffffffee] absolute flex justify-center items-center" v-show="isLoading">
+                <div>
+                    <miao-loading></miao-loading>
+                </div>
+            </div>
+        </Transition>
     </div>
 </template>
 
 <script setup lang="ts">
 
 const route = useRoute();
+
+const iframeRef = ref<HTMLIFrameElement | null>(null);
+
+const isLoading = ref(true);
+
+onMounted(() => {
+    iframeRef.value?.addEventListener('load', () => {
+        // console.log('iframe loaded');
+        isLoading.value = false;
+    })
+})
 
 // onMounted(() => {
 //     console.log(route.params.url);
@@ -15,6 +33,7 @@ const route = useRoute();
 
 <style scoped>
 .iframe-wrapper {
+    position: relative;
     width: 100%;
     min-height: inherit;
     padding: 0;
@@ -22,11 +41,20 @@ const route = useRoute();
     display: flex;
     flex-direction: column;
 }
+
 iframe {
     flex: 1;
     width: 100%;
     /* height: 100%; */
     padding: 0;
     margin: 0;
+}
+
+.fade-leave-active {
+    transition: opacity 0.15s linear;
+}
+
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
