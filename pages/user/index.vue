cd3cd75cd7622392b68a0e22d19ae1cc4cafa3cd
Miao_Spring <539943419@qq.com>
2025-06-10 06:39:36 +0800

    fix: 修复登录逻辑跳转功能相关处理

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -5,18 +5,37 @@
 </template>
 
 <script setup lang="ts">
-
-// definePageMeta({
-//     middleware: [
-//         function (to, from) {
-//             // if (userStore.isLogin === false) {
-//             //     return navigateTo('user/login')
-//             // }
-//             console.log(to, from)
-//         }
-//     ]
-// })
-
+import useUserStore from '~/hooks/pinia/useUserStore';
+// const route = useRoute()
+const router = useRouter()
+const userStore = useUserStore()
+onBeforeMount(async () => {
+    if (userStore && userStore.isLogin) {
+        return router.replace('/user/main')
+    } else {
+        try {
+            if (userStore.token) {
+                const response = await $fetch('/api/user/refreshToken', {
+                    method: 'POST',
+                    body: {
+                        token: userStore.token
+                    }
+                })
+                userStore.setToken(response.token)
+                userStore.setTokenExpireTime(response.expiresIn)
+                userStore.setLoginStatus(true)
+                // // if(route.)
+                // console.log(route)
+                router.replace('/user/main')
+            } else {
+                router.replace('/user/login')
+            }
+        } catch (error) {
+            console.error(error)
+            router.replace('/user/login')
+        }
+    }
+})
 
 </script>
 
