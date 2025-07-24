1fb41c45c09afba7d6cd9345475b1cd6590810e1
Jiahui Zhou <539943419@qq.com>
2025-07-24 18:41:50 +0800

    fix(utils): 修复日期格式化时区问题，强制转换为东八区时间
    将日期格式化函数中的时间计算调整为强制使用东八区时间(UTC+8)，解决时区不一致导致的显示问题

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,15 +1,21 @@
 export default function (date: Date | number, fmt: string) {
+    // 创建Date对象
     date = new Date(date);
+    
+    // 强制转换为东八区时间 (UTC+8)
+    const utcTime = date.getTime() + (date.getTimezoneOffset() * 60000);
+    const chinaTime = new Date(utcTime + (8 * 3600000));
+    
     var o = {
-        "M+": date.getMonth() + 1, //月份 
-        "d+": date.getDate(), //日 
-        "H+": date.getHours(), //小时 
-        "m+": date.getMinutes(), //分 
-        "s+": date.getSeconds(), //秒 
-        "q+": Math.floor((date.getMonth() + 3) / 3), //季度 
-        "S": date.getMilliseconds() //毫秒 
+        "M+": chinaTime.getMonth() + 1, //月份 
+        "d+": chinaTime.getDate(), //日 
+        "H+": chinaTime.getHours(), //小时 
+        "m+": chinaTime.getMinutes(), //分 
+        "s+": chinaTime.getSeconds(), //秒 
+        "q+": Math.floor((chinaTime.getMonth() + 3) / 3), //季度 
+        "S": chinaTime.getMilliseconds() //毫秒 
     };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (chinaTime.getFullYear() + "").substr(4 - RegExp.$1.length));
     for (var k in o) {
         // @ts-ignore
         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
