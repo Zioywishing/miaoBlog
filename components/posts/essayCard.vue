b1afe6efc9482f13fe87c7f3b00a79253f450cee
Jiahui Zhou <539943419@qq.com>
2025-07-24 19:51:24 +0800

    fix(essayCard): 修复图片滚动问题并升级element-plus依赖
    将图片展示从img标签替换为el-image组件以支持预览功能
    修复图片水平滚动时的问题
    升级element-plus到2.10.4版本

--- components/posts/essayCard.vue
+++ components/posts/essayCard.vue
@@ -4,7 +4,7 @@
             <div class="flex items-center gap-1 ">
                 <time-icon style="height: 16px; color: #999999;" />
                 <span class="text-sm text-[#999999]">{{ formatDate(essay.createTime, 'yyyy-MM-dd HH:mm:ss')
-                    }}</span>
+                }}</span>
             </div>
             <div class="flex-1 h-0 border-b-1 border-[#e8e8e8] mx-3"></div>
         </div>
@@ -14,9 +14,16 @@
             </section>
             <div v-if="essay.images && essay.images.length > 0" ref="imgListRef" class="rounded-b-md overflow-hidden">
                 <el-scrollbar ref="scrollbarRef">
-                    <div class="flex gap-2 items-center pb-3">
-                        <img v-for="(image, index) in essay.images" :key="index" :src="image" :alt="`图片${index + 1}`"
-                            loading="lazy" class="h-40 rounded-sm" />
+                    <div class="flex gap-2 items-center pb-3 w-max">
+                        <el-image v-for="(image, index) in essay.images" :key="index" :src="image"
+                            :alt="`图片${index + 1}`" loading="lazy" class="h-40 rounded-sm cursor-pointer"
+                            :preview-src-list="essay.images" :initial-index="index" fit="contain"
+                            style="width: auto; max-width: none;">
+
+                            <template #progress="{ activeIndex, total }">
+                                <span>{{ activeIndex + 1 + '-' + total }}</span>
+                            </template>
+                        </el-image>
                     </div>
                 </el-scrollbar>
             </div>
@@ -43,24 +50,24 @@ onMounted(() => {
         imgListRef.value.addEventListener('wheel', (e) => {
             e.preventDefault();
             e.stopPropagation();
-            
+
             // 获取滚动条实例
             const scrollbar = scrollbarRef.value;
             if (!scrollbar) return;
-            
+
             // 根据滚轮方向调整滚动位置
             let delta = e.deltaY || e.detail;
 
-            if(!delta) return;
+            if (!delta) return;
 
             // delta = (delta > 0 ? 1 : -1) * Math.max(Math.abs(delta), 10);
-            
+
             // 获取当前滚动位置
             const currentScrollLeft = scrollbar.wrapRef?.scrollLeft || 0;
-            
+
             // 计算新的滚动位置
             const newScrollLeft = delta + currentScrollLeft;
-            
+
             // 执行滚动
             scrollbar.setScrollLeft(newScrollLeft);
         });
